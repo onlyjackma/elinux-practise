d0756708860985a47c9253620bd07462153748c7
jack <Jack>
2020-06-12 14:42:19 +0800

    1、修改网络设置脚本和修改源码位置
    Signed-off-by: jack <Jack>

--- qemu-openwrt-a15-virt/run_openwrt.sh
+++ qemu-openwrt-a15-virt/run_openwrt.sh
@@ -1,3 +1,3 @@
-qemu-system-arm -nographic -M virt -m 64 -kernel openwrt-armvirt-32-zImage \
+qemu-system-arm -nographic -M virt -m 64 -kernel ../../openwrt/bin/targets/armvirt/32/openwrt-armvirt-32-zImage \
 	 -net nic,macaddr=52:54:00:55:66:FE -net tap,ifname=tap1,script=no,downscript=no \
-	  -drive file=openwrt-armvirt-32-root.ext4,format=raw,if=virtio -append 'root=/dev/vda rootwait'
+	  -drive file=../../openwrt/bin/targets/armvirt/32/openwrt-armvirt-32-rootfs-ext4.img,format=raw,if=virtio -append 'root=/dev/vda rootwait'
--- qemu-openwrt-a15-virt/set_net.sh
+++ qemu-openwrt-a15-virt/set_net.sh
@@ -1,7 +1,10 @@
 sudo ip tuntap add tap0 mode tap
+sudo ip tuntap add tap1 mode tap
 sudo ip link add br0 type bridge
 sudo brctl addif br0 tap0
+sudo brctl addif br0 tap1
 sudo ifconfig tap0 0.0.0.0
+sudo ifconfig tap1 0.0.0.0
 sudo ifconfig br0 192.168.20.1
 sudo iptables -A POSTROUTING -t nat -o enx000ec6cc38ea -j MASQUERADE
 
